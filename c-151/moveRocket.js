90063cbf5cfcae45affa600704494e53894db47c
Arshit234 <105442468+Arshit234@users.noreply.github.com>
2023-10-22 18:11:34 +0530

    Add files via upload

---
+++ c-151/moveRocket.js
@@ -0,0 +1,13 @@
+AFRAME.registerComponent("move-rocket-on-click", {
+    schema: {
+        moveY: { type: "number", default: 1 }
+    },
+    tick: function () {
+        window.addEventListener("click", (e) => {
+            this.data.moveY = this.data.moveY + 0.05;
+        })
+        var pos = this.el.getAttribute("position");
+        pos.y = this.data.moveY;
+        this.el.setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
+    }
+})
